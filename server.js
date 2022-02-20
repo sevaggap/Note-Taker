7f5eb9937c0c4c4fcc6316a7c9c9445ecbef43cd
sevaggap <sevag.gaprielian@gmail.com>
2022-02-19 19:57:15 -0500

    added helper files to generate random id for notes, created server.js with basic server structures

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const uuid = require('./helpers/uuid');
+const { parse } = require('path');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
