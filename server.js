aee960f9f3d3f33cc604d28f5b78473da110a18a
sevaggap <sevag.gaprielian@gmail.com>
2022-02-19 19:59:33 -0500

    added API routes for a get and post method

--- server.js
+++ server.js
@@ -21,6 +21,48 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json','utf8', (err,data) =>{
+      if (err) {
+          console.error(err);
+      } else {
+          const parsedData = JSON.parse(data);
+          res.json(parsedData)
+          
+      }
+    })
+  });
+  
+  app.post('/api/notes', (req,res) => {
+      console.log(req.body)
+      const newNote = {
+          title: req.body.title,
+          text: req.body.text,
+          id: uuid()
+      }
+  
+      fs.readFile('./db/db.json', 'utf8', (err,data) => {
+          if(err) {
+              console.error(err);
+          } else {
+              const parsedNotes = JSON.parse(data);
+              parsedNotes.push(newNote);
+              fs.writeFile('./db/db.json', JSON.stringify(parsedNotes,null,4), (writeErr) => writeErr ? console.error(err) : console.info('Successfully updated Notes!')
+              );
+          }
+      });
+  
+      fs.readFile('./db/db.json','utf8', (err,data) =>{
+          if (err) {
+              console.error(err);
+          } else {
+              const parsedData = JSON.parse(data);
+              res.json(parsedData)
+              
+          }
+        })
+  }
+  );  
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
