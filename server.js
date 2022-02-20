7f0f924e494c1f05a6a20c9b43df0d0b03d6a67a
sevaggap <sevag.gaprielian@gmail.com>
2022-02-19 19:58:23 -0500

    added routes to HTML files

--- server.js
+++ server.js
@@ -13,6 +13,15 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
