57859d9ce4733d4abb014284b0a71a00d639c075
sevaggap <sevag.gaprielian@gmail.com>
2022-02-19 20:00:48 -0500

    added API route for deleting notes - bonus marks

--- server.js
+++ server.js
@@ -62,7 +62,42 @@ app.get('/api/notes', (req, res) => {
           }
         })
   }
-  );  
+  );
+  
+  app.delete('/api/notes/:id', (req,res) => {
+    if(req.body && req.params.id) {
+        console.info(`${req.method} request received to remove a note`);
+        const noteid = req.params.id;
+        fs.readFile('./db/db.json', 'utf8', (err,data) => {
+            if(err) {
+                console.error(err);
+            } else {
+                const parsedNotes = JSON.parse(data);
+                console.info(parsedNotes);
+                for (i=0; i<parsedNotes.length; i++) {
+                    if(noteid == parsedNotes[i].id) {
+                        console.info(parsedNotes[i]);
+                        const newNotes = parsedNotes.splice(0,i)
+                        console.info(newNotes);
+                        fs.writeFile('./db/db.json', JSON.stringify(newNotes,null,4), (writeErr) => writeErr ? console.error(err) : console.info('Successfully updated Notes!'));
+                    }
+                    
+                }
+            }
+        });
+        fs.readFile('./db/db.json','utf8', (err,data) =>{
+            if (err) {
+                console.error(err);
+            } else {
+                const parsedData = JSON.parse(data);
+                res.json(parsedData)
+                
+            }
+          })
+    }
+}
+);
+
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
